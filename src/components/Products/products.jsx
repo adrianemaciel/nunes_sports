601e5c77ea7e006ac93f420358f74152a68fb59b
Adriane Maciel <adrianemaciel.developer@gmail.com>
2024-09-05 21:55:23 -0300

    feat: dynamic data persistence

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -1,8 +1,100 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./products.css";
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState({
+    id: "",
+    name: "",
+    code: "",
+    description: "",
+    price: "",
+  });
+
+  const fetchProducts = () => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/register", newProduct)
+      .then((res) => {
+        setProducts([...products, res.data]);
+        fetchProducts();
+        setNewProduct({
+          id: "",
+          name: "",
+          code: "",
+          description: "",
+          price: "",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="container-products">
+    <div>
       <h1>Produtos</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Nome do produto"
+          autoComplete="name"
+          value={newProduct.name}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="code">Código:</label>
+        <input
+          type="text"
+          id="code"
+          name="code"
+          placeholder="Código do produto"
+          autoComplete="off"
+          value={newProduct.code}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="description">Descrição:</label>
+        <textarea
+          id="description"
+          name="description"
+          placeholder="Descrição do produto"
+          autoComplete="off"
+          value={newProduct.description}
+          onChange={handleInputChange}
+        ></textarea>
+
+        <label htmlFor="price">Preço:</label>
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Preço do produto"
+          autoComplete="off"
+          value={newProduct.price}
+          onChange={handleInputChange}
+        />
+
+        <button type="submit">Adicionar produto</button>
+      </form>
 
       <table>
         <thead>
@@ -15,26 +107,22 @@ const Products = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>nome</td>
-            <td>codigo</td>
-            <td>descrição</td>
-            <td>preço</td>
-            <td>
-              <button>Editar</button>
-              <button className="button-delete">Deletar</button>
-            </td>
-          </tr>
+          {products.map((product) => {
+            return (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>{product.code}</td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>
+                  <button>Editar</button>
+                  <button className="button-delete">Deletar</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-
-      <form>
-        <input type="text" name="nome" placeholder="Nome"></input>
-        <input type="text" name="codigo" placeholder="Código"></input>
-        <textarea name="descricao" placeholder="Descrição"></textarea>
-        <input type="number" name="preco" placeholder="Preço"></input>
-        <button type="submit">Adicionar produto</button>
-      </form>
     </div>
   );
 };
