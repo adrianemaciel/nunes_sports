7356d8b2376d8f8d52d8089ded0549febb239fff
Adriane Maciel <adrianemaciel.developer@gmail.com>
2024-09-30 17:45:06 -0300

    feat: footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Products from "./components/Products/products";
+import Footer from "./components/Products/footer";
 
 function App() {
   return (
     <>
-      <Products/>
+      <Products />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Products/footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>
+          Desenvolvido por: <strong>Adriane Maciel</strong>
+        </p>
+        <p>
+          Contato:{" "}
+          <a href="mailto:adrianemaciel.developer@gmail.com">
+            adrianemaciel.developer@gmail.com
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
