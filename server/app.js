8aeff8d2eaac131fc8233161579c39c4e2a72ba1
Adriane Maciel <adrianemaciel.developer@gmail.com>
2024-10-02 18:41:28 -0300

    Adding CORS configuration to Netlify

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
 
-app.use(cors({ origin: "https://seu-site-netlify.com" }));
+app.use(cors({ origin: "https://nunes-sports-store.netlify.app/" }));
 app.use(express.json());
 
 app.use("/api", productRoutes);
